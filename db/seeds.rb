b3078fb127d4d41823cb5e6c9ea697c6867b4f5b
Hugo Moraga <root@debian8svm.debian8svm>
2020-03-27 21:36:53 -0400

    fix db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Project.destroy_all
 
 User.create!(email: 'admin@admin.com', username: 'desafiovamoscontodo', password: 'XAHTJEAS23123%23', password_confirmation: 'XAHTJEAS23123%23')
 admin = User.first
